f02da39fad6aecc25dd68c0213fbaaed3c702143
hyeinjeong524 <hyeinjeong524@gmail.com>
2023-04-30 18:29:45 +0900

    Delete old array for feed

--- seminar/src/routes/feed.js
+++ seminar/src/routes/feed.js
@@ -10,9 +10,6 @@ class FeedDB {
         return FeedDB._inst_;
     }
 
-    #id = 1; #itemCount = 1; 
-    #LDataDB = [{ id: 0, title: "test1", content: "Example body" }];
-
     constructor() { console.log("[Feed-DB] DB Init Completed"); }
 
     selectItems = async ( count ) => {
